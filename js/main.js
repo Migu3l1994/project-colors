b6258070ef11883ae243a90043c47fa09e48524a
miguel sifontes, david mendez <joelmendes1202@gmail.com>
2025-04-28 20:23:06 -0300

    MODIFICAR EL DOM PARA COLOCAR EL COLOR DEL FONDO DE LA PAGINA

--- js/main.js
+++ js/main.js
@@ -7,4 +7,7 @@ function changeBackground(){
     color= color+symbols[Math.floor(Math.random() * 16)];
 }
 console.log(color);
+
+document.body.style.background = color;
+document.querySelector("span").innerHTML= color;
 }
